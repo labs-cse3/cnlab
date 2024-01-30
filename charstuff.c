6c0458abeb0ca38dac9697a265018bb3003d2f5c
labs-cse3 <158053784+labs-cse3@users.noreply.github.com>
2024-01-30 09:45:32 +0530

    Update charstuff.c

--- charstuff.c
+++ charstuff.c
@@ -3,22 +3,40 @@
 int main()
 {
 
-    char s[50];
+    char s[5],d[10];
 
     printf("Enter String \n");
     gets(s);
 
     char a[4] = {'D', 'L', 'E', '\0'};
 
-    int n = strlen(s);
+    int n = strlen(s),j=0;
 
     for (int i = 0; i < n; i++)
     {
+
         if (s[i] == 'D' && s[i + 1] == 'L' && s[i + 2] == 'E')
         {
-            strcat(s, a);
+            for(int k = 0;k < 3;k++)
+            {
+                 d[j]=s[i];
+                 i++;
+                 j++;
+            }
+            i = i-3;
+             for(int k = 0;k < 3;k++)
+            {
+                 d[j]=s[i];
+                 i++;
+                 j++;
+            }
+            i = i-2;
+        }
+        else{
+                d[j]=s[i];
+                j++;
         }
     }
 
-    printf("The String is STXDLE %s  DLEETX", s);
+    printf("The String is STXDLE %s  DLEETX", d);
 }
